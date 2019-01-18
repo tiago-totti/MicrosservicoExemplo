43a825e06468cd43cc4f6a095aeb1e101dddcc20
Tiago Moreira Totti <tiagot@dbserver.com.br>
2019-01-17 23:47:36 -0200

    Adiciona filtro de notificacoes

---
+++ src/MicrosservicoExemplo.Infrastructure/Web/Filters/NotificationHandlerFilter.cs
@@ -0,0 +1,36 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Filters;
+using MicrosservicoExemplo.Application;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace MicrosservicoExemplo.Infrastructure.Web.Filters
+{
+    public class NotificationHandlerFilter : IActionFilter
+    {
+        private readonly INotificationContext _notificationContext;
+
+        public NotificationHandlerFilter(INotificationContext notificationContext)
+        {
+            _notificationContext = notificationContext;
+        }
+
+        public void OnActionExecuted(ActionExecutedContext context)
+        {
+
+            ; // var notificationContext = context. .GetService<INotificationContext>();
+
+            if (_notificationContext.Invalid)
+            {
+                context.Result = new UnprocessableEntityObjectResult(_notificationContext.Notifications);
+                //await context.Response.WriteAsync(JsonConvert.SerializeObject());
+            }
+
+        }
+
+        public void OnActionExecuting(ActionExecutingContext context)
+        {
+        }
+    }
+}
