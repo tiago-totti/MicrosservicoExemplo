839b3ac6a4f8f01682bcd99c02a3f53e8662c001
Tiago Moreira Totti <tiagot@dbserver.com.br>
2019-01-21 13:25:10 -0200

    Adiciona monitoramento

--- src/MicrosservicoExemplo.API/Startup.cs
+++ src/MicrosservicoExemplo.API/Startup.cs
@@ -10,13 +10,18 @@ using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Logging;
 using Microsoft.Extensions.Options;
+using MicrosservicoExemplo.Application;
+using MicrosservicoExemplo.Application.UseCases.Contas;
+using MicrosservicoExemplo.Domain.Contas.Repositorios;
+using MicrosservicoExemplo.Infrastructure.Data;
 using MicrosservicoExemplo.Infrastructure.Web;
+using MicrosservicoExemplo.Infrastructure.Web.Filters;
 
 namespace MicrosservicoExemplo.API
 {
     public class Startup
     {
-        private IApplicationStartup _startup;
+        //private IApplicationStartup _startup;
 
         public Startup(IConfiguration configuration)
         {
@@ -28,19 +33,33 @@ namespace MicrosservicoExemplo.API
         // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
         {
-            _startup = services.BuildServiceProvider().GetRequiredService<IApplicationStartup>();
-            _startup.ConfigureServices(services);
+            services.AddSingleton<IContaCorrenteRepositorio, InMemoryRepository>();
+            services.AddScoped<INotificationContext, NotificationContext>();
+            services.AddScoped<ITransferenciaUseCase, TransferenciaUseCase>();
+            services.AddScoped<ICadastrarContaCorrenteUseCase, CadastrarContaCorrenteUseCase>();
+
+            services.AddMvc(options =>
+            {
+                options.Filters.Add<NotificationHandlerFilter>();
+            });
+
+            services.AddMetrics();
+            //_startup = services.BuildServiceProvider().GetRequiredService<IApplicationStartup>();
+            //_startup.ConfigureServices(services);
         }
 
         public void ConfigureContainer(ContainerBuilder builder)
         {
-            _startup.ConfigureContainer(builder);
+            
+            //_startup.ConfigureContainer(builder);
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
         public void Configure(IApplicationBuilder app, IHostingEnvironment env)
         {
-            _startup.Configure(app, env);
+            // app.UseMiddleware<NotificationHandlerMiddleware>();
+            app.UseMvc();
+            //_startup.Configure(app, env);
         }
     }
 }
