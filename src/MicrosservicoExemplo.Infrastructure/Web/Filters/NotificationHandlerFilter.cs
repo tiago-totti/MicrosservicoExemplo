5b814a4c34b028f3a0f43deb1f0a4245eeefadfe
Tiago Moreira Totti <tiagot@dbserver.com.br>
2019-01-21 13:24:53 -0200

    Adiciona filtro para domain notifications

--- src/MicrosservicoExemplo.Infrastructure/Web/Filters/NotificationHandlerFilter.cs
+++ src/MicrosservicoExemplo.Infrastructure/Web/Filters/NotificationHandlerFilter.cs
@@ -1,9 +1,6 @@
 ﻿using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.Filters;
 using MicrosservicoExemplo.Application;
-using System;
-using System.Collections.Generic;
-using System.Text;
 
 namespace MicrosservicoExemplo.Infrastructure.Web.Filters
 {
@@ -18,13 +15,10 @@ namespace MicrosservicoExemplo.Infrastructure.Web.Filters
 
         public void OnActionExecuted(ActionExecutedContext context)
         {
-
-            ; // var notificationContext = context. .GetService<INotificationContext>();
-
+            
             if (_notificationContext.Invalid)
             {
                 context.Result = new UnprocessableEntityObjectResult(_notificationContext.Notifications);
-                //await context.Response.WriteAsync(JsonConvert.SerializeObject());
             }
 
         }
